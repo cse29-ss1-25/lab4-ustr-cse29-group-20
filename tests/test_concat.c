ac12f2fe370c88a067f860b37293a59a2ad325b2
Gulati <sigulati@ieng6-202.ucsd.edu>
2025-07-23 12:31:26 -0700

    new changes for concat testcase

---
+++ tests/test_concat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ustr.h"
+
+/*
+concat two strings 
+*/
+int main() {
+    char input[256];
+    while (fgets(input, sizeof(input), stdin)) {
+        // Remove newline character if present
+        size_t l = strlen(input);
+        if (l > 0 && input[l - 1] == '\n') {
+            input[l - 1] = '\0';
+        }
+	UStr q = new_ustr(input);
+        UStr s = new_ustr(input);
+        UStr r = concat(s,q);
+        print_ustr(r);
+        printf("\n");
+        free_ustr(r);
+        free_ustr(s);
+	free_ustr(q);
+    }
+    return 0;
+}
+
