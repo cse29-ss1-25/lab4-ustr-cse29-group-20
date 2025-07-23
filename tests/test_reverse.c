ae457eb431a41915344c945a7c98ee1e214954f1
Ghosh <arg070@ieng6-203.ucsd.edu>
2025-07-23 11:45:46 -0700

    Functioning reverse in ustr.c

--- tests/test_reverse.c
+++ tests/test_reverse.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 #include "ustr.h"
 
+/*
+rints out the reverse of the input string 
+*/
 int main() {
     char input[256];
-    while(fgets(input, sizeof(input), stdin)) {
+    while (fgets(input, sizeof(input), stdin)) {
         // Remove newline character if present
         size_t l = strlen(input);
         if (l > 0 && input[l - 1] == '\n') {
@@ -12,7 +17,11 @@ int main() {
         }
 
         UStr s = new_ustr(input);
-        printf("len(%s) = %d\n", s.contents, len(s));
+	UStr r = reverse(s);
+        print_ustr(r);
+        printf("\n");
+        free_ustr(r);
         free_ustr(s);
     }
+    return 0;
 }
